2c3070f6131cdf32cc5ba0b208b5b9b8451e2c43
Alexander Klymuk <swcAlexander@gmail.com>
2023-03-01 09:49:11 +0200

    змінює 9 завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-const backgraundEl = document.querySelector('.widget').parentElement;
+
+const backgraundEl = document.querySelector('body');
 const buttonColorEl = document.querySelector('.change-color');
 const spanColorEl = document.querySelector('.color');
 buttonColorEl.addEventListener('click', () => {
   backgraundEl.style.backgroundColor = getRandomHexColor();
   spanColorEl.textContent = getRandomHexColor();
 });
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
